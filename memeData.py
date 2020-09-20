2e5d634bebf91c2478780e3caa5f91368e7e4396
Byungchan Lim <bl458@cornell.edu>
2020-09-20 20:00:06 +0900

    Initial commit

---
+++ memeData.py
@@ -0,0 +1,18 @@
+import json
+from importlib.metadata import version
+import urllib.request
+
+
+url = 'http://localhost:3000/admin/session/'
+body = {"email": "byungchan9707@gmail.com", "pw": "test1234"}
+
+
+data = urllib.parse.urlencode(body)
+data = data.encode('ascii')  # data should be bytes
+req = urllib.request.Request(url, data)
+
+try:
+    res = urllib.request.urlopen(req)
+    print(res.read().decode('utf-8'))
+except Exception as err:
+    print('Error: ', err)
