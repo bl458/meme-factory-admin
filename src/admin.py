68797ec629366ebbc1db04ff2c2784bf2de07f72
Byungchan Lim <bl458@cornell.edu>
2020-09-20 21:44:25 +0900

    fix: Fix admin.py

--- src/admin.py
+++ src/admin.py
@@ -1,18 +1,43 @@
 import json
 import urllib.request
+import requests
+from os import path
 
 
+# Log in to new admin session
 def login_admin(email, pw):
-    # Log in to new admin session
-    url = 'http://localhost:3000/admin/session/'
+    url = 'http://localhost:3000/admin/session'
     body = {"email": email, "pw": pw}
-    body = urllib.parse.urlencode(body).encode('ascii')  # body in bytes
-
-    req = urllib.request.Request(url, body)
 
     try:
-        res = urllib.request.urlopen(req)
-        token = res.read().decode('utf-8')
-        return token
+        response = requests.post(url, data=body)
+        print('Logged in as admin: ', response.status_code)
+
+        return response.text
     except Exception as err:
-        print('Admin login error: ', err)
+        print('Admin Login Error: ', err)
+
+
+# Upload image files
+def upload_files_admin(files, session_token):
+    url = 'http://localhost:3000/admin/images'
+    headers = {'api-token': session_token}
+    files_body = []
+
+    # Initialize body of request
+    for file in files:
+        file_ext = path.splitext(file)[1][1:]
+        files_body.append(('files', (file, open(
+            file, 'rb'), 'image/' + file_ext)))
+
+    try:
+        response = requests.post(url, files=files_body, headers=headers)
+        if response.status_code == 201:
+            print()
+            for i in range(len(files)):
+                print(files[i], response.json()[i]['url'])
+                print()
+        else:
+            print(files, response.json())
+    except Exception as err:
+        print('Admin Image Upload Error: ', err)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-import os
 from dotenv import load_dotenv, find_dotenv
-from memeData import login_admin
+from admin import login_admin, upload_files_admin
+from os import listdir, getenv
 
+# Check gitignore
+IMG_DIR = '../assets'
 
 if __name__ == "__main__":
     # Load from .env
     load_dotenv(find_dotenv())
-    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
-    ADMIN_PW = os.getenv('ADMIN_PW')
+    ADMIN_EMAIL = getenv('ADMIN_EMAIL')
+    ADMIN_PW = getenv('ADMIN_PW')
 
-    print(login_admin(ADMIN_EMAIL, ADMIN_PW))
+    # Login as admin
+    session_token = login_admin(ADMIN_EMAIL, ADMIN_PW)
+
+    # Process files inside assets
+    files = ['../assets/' + i for i in listdir('../assets')]
+
+    # Upload files as admin
+    upload_files_admin(files, session_token)
